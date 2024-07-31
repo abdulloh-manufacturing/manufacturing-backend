d074f5f17c48ffe7df29e5af9ed8fb04d3f5b640
Samandar777777 <msamandarv@gmail.com>
2024-07-31 12:23:20 +0500

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-// import { SharedModule } from "@shared/shared.module";
+import { SharedModule } from "@shared/shared.module";
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
 import { ValumeTypeModule } from './valume-type/valume-type.module';
-import { SharedModule } from "./shared/shared.module";
 
 
 @Module({
