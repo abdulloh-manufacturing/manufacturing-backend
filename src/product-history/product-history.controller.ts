f84048c79783b8357e082012da298f57e42c6753
Samandar777777 <msamandarv@gmail.com>
2024-09-04 15:01:40 +0500

    fix

--- src/product-history/product-history.controller.ts
+++ src/product-history/product-history.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { ProductHistoryService } from './product-history.service';
+import { ListDto } from '@shared/dtos/index.dto';
 
+@ApiTags('product-history')
 @Controller('product-history')
 export class ProductHistoryController {
-//   @Inject() private readonly productHistoryService: ProductHistoryService;
+  @Inject() private readonly productHistoryService: ProductHistoryService;
 
-//   @ApiBody({
-//     type: ProductCreateDto,
-//   })
-//   @Post('create')
-//   async create(@Body() params: ProductCreateDto) {
-//     return this.productHistoryService.create(params);
-//   }
+  @ApiBody({
+    type: ListDto,
+    description: 'list'
+  })
+  @Post('list')
+  async list(@Body() params: ListDto){
+    return this.productHistoryService.list(params)
+  }
 }
